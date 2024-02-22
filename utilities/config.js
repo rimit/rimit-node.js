976a218a6ad6ea3051ee133276d86735519f2eb8
Rimit <106008521+rimit@users.noreply.github.com>
2024-02-22 10:25:56 +0530

    Update config.js

--- utilities/config.js
+++ utilities/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    // FOR MORE INFO CHECK THE DOCUMENT - https://doc.rimit.co/getting-started/readme#rest
-    BASE_URL: 'https://uat-gateway.rimit.co/api/client/rimit/v1', // FOR UAT API
-    // BASE_URL: 'https://api-gateway.rimit.co/api/client/rimit/v1', // FOR PRODUCTION API
+    // FOR MORE INFO CHECK THE DOCUMENT - https://developer.rimit.in/getting-started/readme#rest
+    BASE_URL: 'https://uat-gateway.rimit.in/api/client/rimit/v1', // FOR UAT API
+    // BASE_URL: 'https://api-gateway.rimit.in/api/client/rimit/v1', // FOR PRODUCTION API
 
-    // FOR MORE INFO CHECK THE DOCUMENT - https://doc.rimit.co/getting-started/readme#multi-tenant
+    // FOR MORE INFO CHECK THE DOCUMENT - https://developer.rimit.in/getting-started/readme#multi-tenant
     IS_MULTY_TENANT_PLATFORM: 'NO', // OPTIONS - YES/NO
     MULTY_TENANT_MODE: 'QUERY', // OPTIONS - QUERY/PARAMS
 };
